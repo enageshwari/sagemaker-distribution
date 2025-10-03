165676e2f4dd6eec1a6d585a6ecb967f5b696cd7
Nageshwari Elango <nagelan@amazon.com>
2025-10-03 23:31:22 +0000

    Make get_amazon_q_agentic_chat_artifacts.py file accessible

--- src/config.py
+++ src/config.py
@@ -56,20 +56,6 @@ _image_generator_configs = {
     2: [
         {
             "build_args": {
-                "TAG_FOR_BASE_MICROMAMBA_IMAGE": "jammy-cuda-12.6.0",
-                "CUDA_MAJOR_MINOR_VERSION": "12.6",  # Should match the previous one.
-                "ENV_IN_FILENAME": "gpu.env.in",
-                "PINNED_ENV_IN_FILENAME": "gpu.pinned_env.in",
-                "ARG_BASED_ENV_IN_FILENAME": "gpu.arg_based_env.in",
-            },
-            "additional_packages_env_in_file": "gpu.additional_packages_env.in",
-            "image_tag_generator": "{image_version}-gpu",
-            "env_out_filename": "gpu.env.out",
-            "pytest_flags": ["--use-gpu"],
-            "image_type": "gpu",
-        },
-        {
-            "build_args": {
                 "TAG_FOR_BASE_MICROMAMBA_IMAGE": "jammy",
                 "ENV_IN_FILENAME": "cpu.env.in",
                 "PINNED_ENV_IN_FILENAME": "cpu.pinned_env.in",
